a9632ba261949b5ee033e6a22dba197ec8b8571b
Theo Crithary <tc@apacit.com.au>
2017-06-18 14:45:21 +1000

    v1.0

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ my_uuid = str(uuid.uuid1())
 
 html_header = """<html><head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="/static/apple-touch-icon.png">
     <title>Pied Piper - Lab 1</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
